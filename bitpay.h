27da8b07260df32e9de6a23d66f3c367eb9a9413
Paul Daigle <paul@bitpay.com>
2015-05-05 14:40:43 -0400

    remove external libraries
    add a simple build/test script
    
    Add guide, update readme
    
    Also added the stdint header for cross platform compatibility.

--- bitpay.h
+++ bitpay.h
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "openssl/ec.h"
 #include "openssl/bn.h"
 #include "openssl/sha.h"
---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc -o key_tests -lcrypto tests.c key_utils.c
+./key_tests 
